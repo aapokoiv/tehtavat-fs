6d0245eb8c0e2ef79955ef0d2f62d3a817488824
Aapo Koivula <koivula.aapo@gmail.com>
2026-02-15 15:43:09 +0200

    Part 2c task 2.11 done

--- osa2/phonebook/src/App.jsx
+++ osa2/phonebook/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons.jsx'
 import PersonsForm from './components/PersonsForm.jsx'
 import Filter from './components/Filter.jsx'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [search, setSearch] = useState('')
   
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
   const addPerson = (event) => {
     event.preventDefault()
     const exists = persons.some(person => person.name === newName)
